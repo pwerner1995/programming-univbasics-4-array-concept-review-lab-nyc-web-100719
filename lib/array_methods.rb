499cb455193a562f0d27753efa6ab3321236fe0f
Peter Werner <pcw1995@yahoo.com>
2019-09-04 15:47:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
   not_found = nil 
+  found= FALSE
   array.length.times { |index|
     if array[index] == value_to_find
+      found=TRUE
       return index
     end
     
   }
+  if found == FALSE do
+    return not_found
+  end
 end
 
 def find_max_value(array)
