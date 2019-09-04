fc5175747692cd6c3161d7ac7bd7e28ffc804115
Peter Werner <pcw1995@yahoo.com>
2019-09-04 15:58:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   array.length.times { |index|
     if array[index] == value_to_find
       found = 1 
-      eturn index
+      return index
     end
   }
   if found == 0 do 
